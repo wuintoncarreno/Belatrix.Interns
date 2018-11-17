00fbb9b23b1d8a0df804c92d97ba1f5693992d18
wuintoncarreno <44910935+wuintoncarreno@users.noreply.github.com>
2018-11-17 12:47:44 -0500

    Version Clase 2 c#

---
+++ Belatrix.Repository.SqlServer/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.Repository.SqlServer
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public int Add(T entity)
+        {
+            _dbContext.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public bool Delete(T entity)
+        {
+            _dbContext.Remove(entity);
+            return _dbContext.SaveChanges() > 0;
+        }
+
+        public IEnumerable<T> GetList()
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public bool Update(T entity)
+        {
+            _dbContext.Update(entity);
+            return _dbContext.SaveChanges() > 0;
+        }
+    }
+}
---
+++ Belatrix.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Belatrix.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        int Add(T entity);
+        IEnumerable<T> GetList();
+        bool Update(T entity);
+        bool Delete(T entity);
+    }
+}
